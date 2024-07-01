50f19fc955ad3240ff9f9d82aa11b760acb3fa7b
Uiratan Cavalcante <uiratan@gmail.com>
2024-07-01 16:59:55 -0300

    using cobra for CLI

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.7
 	github.com/satori/go.uuid v1.2.0
+	github.com/spf13/cobra v1.8.1
 	github.com/stretchr/testify v1.7.0
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- main.go
+++ main.go
@@ -1,19 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-
-	_ "github.com/mattn/go-sqlite3"
-	db2 "github.com/uiratan/fullcycle-archdev-hexagonal/adapters/db"
-	"github.com/uiratan/fullcycle-archdev-hexagonal/application"
-)
+import "github.com/uiratan/fullcycle-archdev-hexagonal/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3", "sqlite.db")
-	productDbAdapter := db2.NewProductDb(db)
-	productService := application.NewProductService(productDbAdapter)
-
-	product, _ := productService.Create("Product 1", 10)
-
-	productService.Enable(product)
+	cmd.Execute()
 }
